83dec17312ec91b484700b0f3e409caa1b49bf65
adomasgaudi <adomas.gaudi@gmail.com>
2025-10-14 17:57:07 +0300

    va4

--- sl_analyser/main.py
+++ sl_analyser/main.py
@@ -5,7 +5,7 @@ from variables import NAME_TO_USERNAME, EXERCISE_DATA
 from datetime import datetime, timedelta
 
 # --- Mobile-friendly styling ---
-st.write("v.a3")
+st.write("v.a4")
 st.markdown("""
 <style>
 /* Make select boxes shrink to content width instead of 100% */
@@ -507,5 +507,5 @@ if history_rows:
             template="plotly_white",
         )
         st.plotly_chart(fig, use_container_width=True)
-        st.write("v.a3")
+        st.write("v.a4")
 
